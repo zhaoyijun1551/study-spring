adb8e441dc8d35b7994aceb5cde2b3c0c119a3c0
zhaoyijun1551 <zhj1551@163.com>
2022-03-19 22:43:57 +0800

    springboot整合swagger，使用代码生成器

---
+++ springboot-swagger/src/main/java/com/test/zyj/tiny/service/impl/PmsBrandServiceImpl.java
@@ -0,0 +1,52 @@
+package com.test.zyj.tiny.service.impl;
+
+import com.github.pagehelper.PageHelper;
+import com.test.zyj.tiny.mbg.mapper.PmsBrandMapper;
+import com.test.zyj.tiny.mbg.model.PmsBrand;
+import com.test.zyj.tiny.mbg.model.PmsBrandExample;
+import com.test.zyj.tiny.service.PmsBrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * PmsBrandService实现类
+ */
+@Service
+public class PmsBrandServiceImpl implements PmsBrandService {
+    @Autowired
+    private PmsBrandMapper brandMapper;
+
+    @Override
+    public List<PmsBrand> listAllBrand() {
+        return brandMapper.selectByExample(new PmsBrandExample());
+    }
+
+    @Override
+    public int createBrand(PmsBrand brand) {
+        return brandMapper.insertSelective(brand);
+    }
+
+    @Override
+    public int updateBrand(Long id, PmsBrand brand) {
+        brand.setId(id);
+        return brandMapper.updateByPrimaryKeySelective(brand);
+    }
+
+    @Override
+    public int deleteBrand(Long id) {
+        return brandMapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public List<PmsBrand> listBrand(int pageNum, int pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        return brandMapper.selectByExample(new PmsBrandExample());
+    }
+
+    @Override
+    public PmsBrand getBrand(Long id) {
+        return brandMapper.selectByPrimaryKey(id);
+    }
+}
